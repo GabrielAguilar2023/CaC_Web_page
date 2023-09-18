33578cace584d8a57571bbc1e11ba039ea30018d
GabrielAguilar2023 <el_ayhudante@hotmail.com>
2023-09-18 14:13:20 -0300

    Enhancement in slider control.

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,9 +1,21 @@
+var mover=true;
 var a = 2;
-setInterval(
-	function () {
-		document.getElementById('radio' + a).checked = true;
-		a++;
-		if (a > 3) {
-			a = 1
-		}
-	}, 11000);
+var x = 12000;
+
+document.getElementById('radio1').checked = true;
+
+document.getElementById('manual-activation').addEventListener('click',(e)=>{
+	mover=false;
+})
+
+
+setInterval(	
+		function () {
+			if(mover){
+				document.getElementById('radio' + a).checked = true;
+				a++;
+				if (a > 3) {
+					a = 1
+				}
+			}
+		}, x);
