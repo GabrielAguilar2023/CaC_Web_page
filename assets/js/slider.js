1fd7af96cbac85adf01db872527c2dc29c55a751
GabrielAguilar2023 <el_ayhudante@hotmail.com>
2023-09-19 19:49:13 -0300

    Purchase ticket page added

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,21 +1,22 @@
-var mover=true;
+var move=true;
 var a = 2;
 var x = 12000;
 
+// coloca la primera imagen en el background 
 document.getElementById('radio1').checked = true;
 
+// Desactiva la circulacion de imagenes en background al presionar los botones de control
 document.getElementById('manual-activation').addEventListener('click',(e)=>{
 	mover=false;
 })
 
-
-setInterval(	
-		function () {
-			if(mover){
-				document.getElementById('radio' + a).checked = true;
-				a++;
-				if (a > 3) {
-					a = 1
-				}
-			}
-		}, x);
+// Circulacion de imagenes en background cada x milisegundos
+setInterval(function () {
+	if(move){
+		document.getElementById('radio' + a).checked = true;
+		a++;
+		if (a > 3) {
+		a = 1
+		}
+	}
+}, x);
