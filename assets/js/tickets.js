15ae211b5a00c0e5f359a89da0838ff22dfa96b2
GabrielAguilar2023 <el_ayhudante@hotmail.com>
2023-10-15 19:36:45 -0300

    verification of input data (first steps)

--- assets/js/tickets.js
+++ assets/js/tickets.js
@@ -1,6 +1,6 @@
 /*
     payTextFormat: Una nueva clase que se agrega al elemento con el id="showTotal" para cambiar su formato
-    cuando el importe a pagar es mayor que cero. 
+    cuando el importe a pagar es mayor que cero.
 */
 
 //Extrae el precio desde el texto mostrado en la pagina utilizando expresiones regulares
@@ -14,7 +14,7 @@ const estudiante = document.getElementsByClassName("card")[1];
 
 function erase(){
     showTotal.innerText = `Total a pagar:`;
-    showtotal.classList.remove("payTextFormat"); 
+    showtotal.classList.remove("payTextFormat");
 }
 
 function calculate(){
@@ -34,8 +34,19 @@ document.getElementById('borrarButton').addEventListener("click",erase);
 estudiante.addEventListener("click",()=>{alert(123)})
 
 
-
-
 /******** Validacion de las entradas del formulario ********/
-document.querySelector("#nombre").style.borderColor = "red" ;
-document.querySelector(".formValidationState").classList.remove("cac-error");
+function checkError (input){
+    document.querySelector(`#${input}`).style.borderColor = "red" ;
+    document.querySelector(`.validation-${input}`).classList.add("cac-error");
+    document.querySelector(`.error-${input}`).style.display ="block";
+}
+
+function checkOk (input){
+    document.querySelector(`#${input}`).style.borderColor = "green" ;
+    document.querySelector(`.validation-${input}`).classList.add("cac-ok");
+    document.querySelector(`.error-${input}`).style.display ="none";
+}
+checkOk("name");
+checkOk("surName");
+checkOk("eMail");
+checkOk("numberTickets");
